21f0060f6aaa6c675c3c4932b20ca3a4bab3b7aa
Luca Ciancanelli <luca.ciancanelli1785@gmail.com>
2016-10-24 09:25:36 -0600

    working tic tac toe game

---
+++ application.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	var counter = 0;
+	$('.box').click(function(){
+		counter = counter + 1;
+	});
+
+	$('.box').click(function(){
+		if (counter%2 == 1){
+			$(this).addClass('red');
+		} else {
+			$(this).addClass('black');
+		}
+	});
+});
