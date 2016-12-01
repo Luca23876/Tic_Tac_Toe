c449e3e6afc58309fca5dfb10ff21a24a3648b08
Luca Ciancanelli <luca.ciancanelli1785@gmail.com>
2016-12-01 09:27:41 -0700

    added new photos for x's and o's

--- application.js
+++ application.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
 
 	$('.box').click(function(){
 		if (counter%2 == 1){
-			$(this).addClass('red');
+			$(this).addClass('x');
 		} else {
-			$(this).addClass('black');
+			$(this).addClass('o');
 		}
 	});
 });
